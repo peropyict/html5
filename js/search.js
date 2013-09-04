7e3ebac6673c5f845435dd076045d2e3d1daf83f
peropyict <blatnjak@gmail.com>
2013-09-04 23:31:34 +0200

    + customersearch ajax request

--- js/search.js
+++ js/search.js
@@ -3,41 +3,31 @@ function customerSearch(){
 	$("#resultContainers").empty();
 	var totalResultsNumber = "";
 	$("#noOfSearchResult").empty();
-
 	
 	
-	 /*$.ajax({
+	/*****some local tests*****/
+	/*$.getJSON("json/test/json.json", function(json) {		
+			console.log(json);
+	});
+	$.getJSON("json/test/jsonp.json", function(jsonp) {		
+			console.log(jsonp);
+	});*/
+	/*********end***********/
+	
+	 $.ajax({
         type:"POST",
-        url: "http://gcs.ventiv.com.au/gcs/v1/" + "customersearch.json",
-        data: { orgName: "MORGAN", systemType: "MUREXID"},
+        url: "http://gcs.ventiv.com.au/gcs/v1/" + "customersearch.jsonp",
+		jsonpCallback: "customersearchCallback",
+        data: { "orgName": "MORGAN", "systemType": "MUREXID"},
         contentType:"application/json",
-		crossDomain:true,
         beforeSend: function(jqXHR) {
             jqXHR.setRequestHeader("X-Requested-With","XMLHttpRequest");
         },
-        dataType:"jsonp",
-        success: function (data) {
-            console.log("success");
-            console.log(data);
-        }
-       
-    }).done(function(data) {
-        console.log("Data Loaded: " + data);
-
-        if(json.success == true){
-            for(var i=0; i < json.results.length; i++){
-				writeCustomerSearchResults(json.results[i]);
-			}
-			if(json.results.length > 0)
-				totalResultsNumber = "1 - " + json.results.length +" Results Found";
-			else
-				totalResultsNumber = "No results found";
-			$("#noOfSearchResult").append(totalResultsNumber);
-         }
-    });*/
+        dataType:"jsonp" 
+    });
 	
 	
-	$.getJSON("json/customersearch.json", function(json) {
+	/*$.getJSON("json/customersearch.json", function(json) {
 		if(json.success == true){
 			for(var i=0; i < json.results.length; i++){
 				writeCustomerSearchResults(json.results[i]);
@@ -48,8 +38,21 @@ function customerSearch(){
 				totalResultsNumber = "No results found";
 			$("#noOfSearchResult").append(totalResultsNumber);
 		}
-	});
+	});*/
 	
+}
+function customersearchCallback(json){
+	//console.log(json);
+	if(json.success == true){
+            for(var i=0; i < json.results.length; i++){
+				writeCustomerSearchResults(json.results[i]);
+			}
+			if(json.results.length > 0)
+				totalResultsNumber = "1 - " + json.results.length +" Results Found";
+			else
+				totalResultsNumber = "No results found";
+			$("#noOfSearchResult").append(totalResultsNumber);
+         }
 }
 /*TODO null instead plus button, if doesn't contain members*/
 function writeCustomerSearchResults(node){
@@ -119,16 +122,13 @@ function writeCustomerOverviewRows(node, entityId){
 function CustomerOverviewMemberDetails(entityId){
 	/*there should be code for ajax memberdetails call with provided systemId or srcCode?*/	
 	$.each($(".ProfileRowsContainer"), function(e){
-		//console.log(($(this).find("div.COMembersPlus")).attr("id"));
 		var parentId = $(this).attr("id");
-		//console.log(parentId);
 		$.getJSON("json/memberdetails.json", function(json) {
 			if(json.success == true){
 				writeCustomerOverviewMemberDetails(parentId, json.entity, entityId);
 			}
 		});		
 	})	
-	//($(this).find("div.COMembersPlus")).attr("id") ------ this param should be used during memberdetails.json quering? check it!	
 }
 function writeCustomerOverviewMemberDetails(parentId, node, entityId){
 	var output = $('#popupSubRowsTemplate').parseTemplate(node);
